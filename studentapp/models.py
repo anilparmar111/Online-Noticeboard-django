f70c4e73196110b50242c4b7e4499e4b1a3899e0
anilparmar111 <53622176+anilparmar111@users.noreply.github.com>
2021-02-10 19:30:29 +0530

    Update models.py

--- studentapp/models.py
+++ studentapp/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+#hear i have created diffrent model model for diffrent department because i use normalization
+#we can also create 1 table for all table like this 
+# class faculty(models.model):
+#     id
+#     sem
+#     uptime
+#     userid
+#     location
+#     branch // for diffrent branch's faculty
+#     but if we thousands of data then in this table
+#     we have redundant data in branch column 
+#     so i use difrent diffrent database table for diffrent diffrent branch
+#     otherwise i can create 1 table for all the facultys
 
 class Ce_reminder(models.Model):
     userid = models.CharField(max_length=11)
